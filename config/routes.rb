405bb366744764427dbebe1071b2468db812757d
jlmansilla <juanmancillaster@gmail.com>
2022-09-29 19:53:14 -0300

    cors habilitation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   api_version(:module => "Api::V1", :path => {:value => "api/v1"}, :defaults => {:format => "json"}) do
-    end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :users
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+# #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Creating tables"
-Array((1..10)).each do |table|
-  Table.create(
-    {
-      number: table
-    }
-  )
-end
+# puts "Creating tables"
+# Array((1..10)).each do |table|
+#   Table.create(
+#     {
+#       number: table
+#     }
+#   )
+# end
 
-puts "Creating Meals"
-dishes = %w[porotos arroz tallarines pollo chapsui pizza lasagna curanto cancato salmón]
-dishes.each do |meal|
-  Meal.create(
-    {
-      name: meal
-    }
-  )
-end
+# puts "Creating Meals"
+# dishes = %w[porotos arroz tallarines pollo chapsui pizza lasagna curanto cancato salmón]
+# dishes.each do |meal|
+#   Meal.create(
+#     {
+#       name: meal
+#     }
+#   )
+# end
 
-puts "Creating Checks"
-10.times do
-  Check.create(
-    {
-      tables_id: rand(9) + 1,
-      meals_id: rand(9) + 1
-    }
-  )
-end
+# puts "Creating Checks"
+# 10.times do
+#   Check.create(
+#     {
+#       tables_id: rand(9) + 1,
+#       meals_id: rand(9) + 1
+#     }
+#   )
+# end
+
+User.create(name: 'John', likes:1)
