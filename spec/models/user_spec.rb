444b3a91728f530c3407b8054542e2e141ff83f1
Marilyn Aguilar González <aguilargonzalezmary@outlook.com>
2022-10-04 22:04:27 -0300

    users_routing_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'database_cleaner'
 
 describe User do
   context 'when create a user' do
---
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :routing do
+  describe "routing" do
+    let(:controller){'users'}
+
+    # >> GET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        
+    include_examples 'Routes to index'
+
+    include_examples 'Routes to show'
+    
+    # >> POST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+       
+    include_examples 'Routes to create'
+
+    # >> PATCH/PUT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
+    include_examples 'Routes to put'
+
+    # >> DELETE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    include_examples 'Routes to delete'
+  end
+end
